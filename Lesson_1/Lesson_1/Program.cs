94f9a815765070765de240393c866942bdebadad
blackskyfpv <ilyabudnikxs@mail.ru>
2020-09-06 20:34:53 +0300

    Version 1

---
+++ Lesson_1/Lesson_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше Имя: "); // Вывод сообщения и чтение данных введённых в консоль(строку)
+            string name = Console.ReadLine(); //Чтение только что записаных данных в консоль и запись в пременную name
+            Console.WriteLine("Привет:" + name); // конкатинация сообщения и строчной переменной
+        }
+    }
+}
