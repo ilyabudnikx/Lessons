e328a920d12b9f725189a1373c0794ff87099247
blackskyfpv <ilyabudnikxs@mail.ru>
2020-09-07 08:01:00 +0300

    char problem

---
+++ Test/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1 = 'a';
+            char c2 = 'c';
+            char c3 = c1 + c2; //why???
+        }
+    }
+}
